931df13a0ec35cea076d6b3231873af20fb8d4ab
karim369 <karim.chakroun@outlook.com>
2021-07-08 15:06:32 +0200

    commit1

---
+++ WebApplication1/WebApplication1/Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int? Id { get; set; }
+        public string NameT { get; set; }
+        public string Status { get; set; }
+        public string DateT { get; set; }
+        public string Description { get; set; }
+    }
+}
